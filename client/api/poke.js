a541d3f5e330ec290f53f670d239e18f7defec23
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-05-13 22:42:11 +1200

    changing api call function wip

--- client/api/poke.js
+++ client/api/poke.js
@@ -3,3 +3,28 @@ import request from "superagent"
 export function getAllPokemon(){
     return request.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
 }
+
+export function getPokemonPage1(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?limit=20')
+}
+export function getPokemonPage2(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
+}
+export function getPokemonPage3(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=40&limit=20')
+}
+export function getPokemonPage4(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=60&limit=20')
+}
+export function getPokemonPage5(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=80&limit=20')
+}
+export function getPokemonPage6(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=100&limit=20')
+}
+export function getPokemonPage7(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset=120&limit=20')
+}
+export function getPokemonPage8(){
+    return request.get('https://pokeapi.co/api/v2/pokemon/?offset140&limit=20')
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,55 @@
 import React from 'react'
-import {getAllPokemon} from '../api/poke'
+import {getAllPokemon, getPokemonPage1} from '../api/poke'
 
 class App extends React.Component  {
   constructor(props) {
     super(props)
     this.state = {
-      pokemon: []
+      pokemon: [],
+      page: 1,
+      defaultPage: 1
     }
   }
+
+  
 
   componentDidMount() {
     console.log('did mount')
 
-    getAllPokemon()
-    .then(pokemon => {
-      const pokelist = pokemon.body.results.map((poke) => {return poke.name})
-      
-      this.setState({
-        pokemon: pokelist
-      })
-    })
-    
+    // getAllPokemon()
+    // .then(pokemon => {
+    //   const pokelist = pokemon.body.results
+    //   this.setState({
+    //     pokemon: pokelist
+    //   })
+    // })
+
+  
+
+    // getPokemonPage1() 
+    //   .then(pokemon => {
+    //     let pokelist = pokemon.body.results
+    //     this.setState({
+    //       pokemon: pokelist
+    //     })
+    //   })
+
+   console.log(this.state.page)
   }
+       
+        
+
 
 
   render() {
-    let pokedex = this.state.pokemon
+    let pokedex = this.state.pokemon.map((poke) => {return poke.name})
+    
+    function  callPokemon  {
+      let page = this.state.defaultPage
+      page = page + this.state.page
+      this.setState({ page })
+     }
+
   return (
     <>
     <h1>Pokedex</h1>
@@ -33,6 +57,7 @@ class App extends React.Component  {
         <ul>
       {pokedex.map((pokemon) => <li key={pokemon}>{pokemon}</li> )}
       </ul>
+      <button onClick={this.callPokemon.bind(this)}></button>
       </div>
     </>    
 
