40b4933d67dfe62ae5f43541c34abfc6fc01db2d
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-05-20 11:09:58 +1200

    page btn restrictions

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,75 +1,21 @@
 import React from 'react'
-import {getAllPokemon, getPokemonPage1} from '../api/poke'
-import { incrementPage } from '../actions'
+
+import Pokedex from './Pokedex'
 
 class App extends React.Component  {
   constructor(props) {
     super(props)
     this.state = {
-      pokemon: [],
-      startItem: 0,
-      showItems: 20,
+     
     }
-    this.handleShowMore = this.handleShowMore.bind(this)
-    this.handleShowLess = this.handleShowLess.bind(this)
-
   }
 
-  
-
-  componentDidMount() {
-    console.log('did mount')
-
-    getAllPokemon()
-    .then(pokemon => {
-      const pokelist = pokemon.body.results
-      this.setState({
-        pokemon: pokelist
-      })
-      console.log(this.state)
-    })
-  }
-      handleShowMore() {
-        this.setState({
-          showItems:
-            this.state.showItems = this.state.showItems + 20,
-          startItem:
-            this.state.startItem = this.state.startItem + 20
-        })
-      }
-
-      handleShowLess() {
-        this.setState({
-          showItems:
-          this.state.showItems = this.state.showItems - 20,
-          startItem:
-          this.state.startItem = this.state.startItem - 20
-        })
-      }
-
-    
-    
-  
   render() {
-    let pokedex = this.state.pokemon.map((poke) => {return poke.name})
-    
-
   return (
     <>
     <h1>Pokedex</h1>
-      <div className="list">
-    
-      {pokedex.slice(this.state.startItem, this.state.showItems).map((pokemon) => <li key={pokemon}>{pokemon}</li> )}
-       
-        <button onClick={this.handleShowLess}>
-          back
-        </button>
-        <button onClick={this.handleShowMore} >
-          next
-        </button>
-      </div>
+      <Pokedex />
     </>    
-
     )
   }
 }
---
+++ client/components/Pokedex.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {getAllPokemon} from '../api/poke'
+
+class Pokedex extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state ={
+          pokemon: [],
+          startPokemon: 0,
+          lastPokemon: 20,
+      }
+      this.handleNextPage = this.handleNextPage.bind(this)
+      this.handlePrePage = this.handlePrePage.bind(this)
+  }
+
+  componentDidMount() {
+      console.log('did mount')
+  
+      getAllPokemon()
+      .then(pokemon => {
+        const pokelist = pokemon.body.results
+        this.setState({
+          pokemon: pokelist
+        })
+        console.log(this.state)
+      })
+    }
+
+    handleNextPage() {
+      if(this.state.lastPokemon < 160)
+      this.setState({
+        lastPokemon:
+          this.state.lastPokemon = this.state.lastPokemon + 20,
+        startPokemon:
+          this.state.startPokemon = this.state.startPokemon + 20
+      })
+      console.log(this.state.lastPokemon)
+    }
+
+    handlePrePage() {
+      if(this.state.startPokemon > 0)
+      this.setState({
+        lastPokemon:
+        this.state.lastPokemon = this.state.lastPokemon - 20,
+        startPokemon:
+        this.state.startPokemon = this.state.startPokemon - 20
+      })
+    }
+  
+  render() {
+      let pokedex = this.state.pokemon.map((poke) => {return poke.name})
+  
+  return (
+  <>
+  <h1>Pokedex</h1>
+    <div className="list">
+
+    {pokedex.slice(this.state.startPokemon, this.state.lastPokemon).map((pokemon) => <li key={pokemon}>{pokemon}</li> )}
+    
+    <button onClick={this.handlePrePage}>
+      back
+    </button>
+    <button onClick={this.handleNextPage} >
+      next
+    </button>
+    </div>
+  </>    
+
+  )
+  }
+}
+
+export default (Pokedex)
