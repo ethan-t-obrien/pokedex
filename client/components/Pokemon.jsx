65b3e191dd65f8bbfee8159e2e95a9b606bc03b8
Ethan O'Brien <ethan@MacBook-Pro.fritz.box>
2020-06-21 00:26:36 +1200

    showing poke information and pics

--- client/components/Pokemon.jsx
+++ client/components/Pokemon.jsx
@@ -6,7 +6,7 @@ class Pokemon extends React.Component {
         super(props)
         this.state = {
             pokeName: {},
-            pokeInfo: {
+            pokeBody: {
 
             }
         }
@@ -14,24 +14,49 @@ class Pokemon extends React.Component {
 
     componentDidMount() {
 
-        let pokemon = this.props.match.params.pokemon
-        console.log( 'hi', pokemon)
+        let pokeName = this.props.match.params.pokemon
+        console.log( 'hi', pokeName)
         // this.setState({
         //     pokeName: pokemon
         // })
         // console.log(this.state)
-        getPokemon(pokemon)
-            .then(pokemon => {
-                const pokeInfo = pokemon
-                console.log(pokeInfo)
-            })
+        getPokemon(pokeName)
+            .then(pokeName => {
+								const pokeInfo = pokeName.body
+								console.log(pokeInfo)
+								this.setState({
+									pokeBody: {
+										name: pokeInfo.name,
+										height: pokeInfo.height,
+										weight: pokeInfo.weight,
+										no: pokeInfo.id,
+										type: pokeInfo.types[0].type.name,
+										img: pokeInfo.sprites.front_default
+									}
+								})
+								if(pokeInfo.types < 1)
+								this.setState({
+									type2: pokeInfo.types[1].type.name
+								})
+
+						})
+						
     }
 
 
 
         render() {
+					console.log(this.state)
+
             return(
-                <h1>reee</h1>
+                <>
+										<img src={this.state.pokeBody.img} alt=""/>
+                    <h3>Name: {this.state.pokeBody.name}</h3>
+										<h3>Height: {this.state.pokeBody.height} m</h3>
+										<h3>Weight: {this.state.pokeBody.weight} kgs</h3>
+										<h3>No. {this.state.pokeBody.no}</h3>
+										<h3>Type: {this.state.pokeBody.type}, {this.state.pokeBody.type2}</h3>
+                </>
             )
         }
 
