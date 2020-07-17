8627663d67dee599d7c5a7330b17a2733770474a
Ethan O'Brien <ethan@MacBook-Pro.fritz.box>
2020-07-17 19:14:31 +1200

    fixed weight display ready for styling

--- client/components/Pokemon.jsx
+++ client/components/Pokemon.jsx
@@ -40,20 +40,33 @@ class Pokemon extends React.Component {
 								})
 
 						})
-						
-    }
+                    }
+                    
+                    
+                    
+                    
+                    render() {
+                        let weight = String(this.state.pokeBody.weight)
+                         console.log(weight.length)
+                        // note to self, trying to add . to kg number, but can't seem to slice 'objects string' ???
 
-
-
-        render() {
-					console.log(this.state)
+                        // let str = 'please just work'
+                        // let newStr = str.slice(0, 3)
+                        // console.log(newStr)
+                        
+                        // let newWeight = weight.slice(0, 1)
+                        // let = weight.splice(weight.length-1, 0, '.')			
+            // const weight = this.state.pokebody.weight
+            // weight.splice(weight.length-1, 0, '.')
+            //         console.log(this.state)
+                    
 
             return(
                 <>
 										<img src={this.state.pokeBody.img} alt=""/>
                     <h3>Name: {this.state.pokeBody.name}</h3>
-										<h3>Height: {this.state.pokeBody.height} m</h3>
-										<h3>Weight: {this.state.pokeBody.weight} kgs</h3>
+										<h3>Height: {this.state.pokeBody.height + '0'} cm</h3>
+										<h3>Weight: {weight.slice(0 , weight.length -1)}.{weight.slice(-1)} kgs</h3>
 										<h3>No. {this.state.pokeBody.no}</h3>
 										<h3>Type: {this.state.pokeBody.type}, {this.state.pokeBody.type2}</h3>
                 </>
